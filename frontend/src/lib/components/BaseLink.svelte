58369725cf60fb54d04fcdc72b04706673479523
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-07-04 12:37:02 +0100

    feat(frontend): add base link component
    Use this instead of a to automatically add the base url to links

---
+++ frontend/src/lib/components/BaseLink.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { base } from '$app/paths';
+
+	/** @type {string} */
+	export let href;
+
+	/** @type {string} */
+	export let className = '';
+
+	// ensure the href starts with a forward slash if it doesn't already
+	$: processedHref = href.startsWith('/') ? `${base}${href}` : `${base}/${href}`;
+</script>
+
+<a href={processedHref} class={className} {...$$restProps} on:click>
+	<slot />
+</a>
