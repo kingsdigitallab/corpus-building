f8236791dd81b9b8fd341e36e741958fd57c914d
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-07-04 11:56:10 +0100

    build(sveltekit): log prerender errors

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { config as dotenv } from 'dotenv';
-import { appendFileSync, writeFileSync } from 'fs';
+import { existsSync, readFileSync, writeFileSync } from 'fs';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
@@ -11,26 +11,35 @@ dotenv({ path: join(__dirname, '.env') });
 
 const debug = process.env.DEBUG === 'true';
 
+const errorFilePath = 'src/lib/prerender-errors.json';
+if (!existsSync(errorFilePath)) {
+	writeFileSync(errorFilePath, '{}');
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
 		prerender: {
 			handleHttpError: ({ path, referrer, message }) => {
-				// don't throw errors in dev mode, just log them
+				// don't throw errors in debug mode, just log them
 				if (debug) {
-					const logMessage = `[${new Date().toISOString()}] ${message}\n`;
+					const errorLog = {
+						timestamp: new Date().toISOString(),
+						path,
+						referrer,
+						message
+					};
 
 					if (path.endsWith('.jpg') || path.endsWith('.tif')) {
-						console.warn(logMessage);
+						// console.warn(message);
 						return;
 					}
 
-					try {
-						appendFileSync('prerender-errors.log', logMessage);
-					} catch (e) {
-						writeFileSync('prerender-errors.log', logMessage);
-					}
+					const errors = JSON.parse(readFileSync(errorFilePath, 'utf-8'));
+					errors[message] = errorLog;
+
+					writeFileSync(errorFilePath, JSON.stringify(errors, null, 2));
 
 					console.warn(`[prerender] Logged error for: ${path}`);
 
