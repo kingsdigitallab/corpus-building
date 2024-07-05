23361ba6436628219d8de11c2666a547f3ed1b67
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-07-05 12:11:55 +0100

    test(playwright): increase the timeouts for building and individual tests
    The build can be slow because of the missing pages, and the Axe tests are also slow to run.

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -1,11 +1,22 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+		command: `npm run build && npm run preview`,
+		port: 4173,
+		timeout: 5 * 60 * 1000
 	},
 	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+	timeout: 5 * 60 * 1000
 };
 
 export default config;
