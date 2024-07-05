4817f2bc8dc9ce0b6e5fed4bb58c1a8dfd0befb6
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-07-05 16:16:42 +0100

    test(frontend): output axe test violations to a file

--- frontend/tests/home.axe.spec.js
+++ frontend/tests/home.axe.spec.js
@@ -1,11 +1,22 @@
 import { expect, test } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
+import fs from 'fs/promises';
+import path from 'path';
 
 test.describe('home.axe', () => {
 	test('home page is accessible', async ({ page }) => {
 		await page.goto('/');
 
 		const results = await new AxeBuilder({ page }).analyze();
+
+		if (results.violations.length > 0) {
+			const violations = { '/': results.violations };
+
+			const outputDir = 'test-results';
+			await fs.mkdir(outputDir, { recursive: true });
+			const filePath = path.join(outputDir, 'home.axe.json');
+			await fs.writeFile(filePath, JSON.stringify(violations, null, 2));
+		}
 
 		expect(results.violations).toEqual([]);
 	});
