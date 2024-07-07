81f5d756ed2baafebfcfc78520c953761e14aa98
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-07-07 21:41:09 +0100

    feat(frontend): add image server setting

--- frontend/src/lib/config.js
+++ frontend/src/lib/config.js
@@ -1,5 +1,10 @@
 import { dev } from '$app/environment';
 
+export const publicUrl = 'http://sicily.classics.ox.ac.uk/';
+
 export const title = 'ISicily';
 export const description = 'Brief description or overview of the ISicily publication.';
-export const url = dev ? 'http://localhost:5173/' : 'https://sicily.classics.ox.ac.uk/';
+export const url = dev ? 'http://localhost:5173/' : publicUrl;
+
+export const imageServer =
+	'https://apheleia.classics.ox.ac.uk/iipsrv/iipsrv.fcgi?IIIF=/inscription_images/';
