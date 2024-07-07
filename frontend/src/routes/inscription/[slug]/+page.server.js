cf9ac533793a4f26de721cb76867a561888f668a
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-07-07 21:45:47 +0100

    feat(frontend): load inscription metadata

--- frontend/src/routes/inscription/[slug]/+page.server.js
+++ frontend/src/routes/inscription/[slug]/+page.server.js
@@ -1,10 +1,14 @@
 import { base } from '$app/paths';
 import { error } from '@sveltejs/kit';
+import corpus from '../../../data/corpus.json';
 
 /** @type {import('../$types').PageServerLoad} */
-export async function load({ params }) {
+export async function load({ params, parent }) {
 	try {
-		const module = await import(`../../../data/html/${params.slug}.json`);
+		const slug = params.slug;
+		const metadata = corpus.find((/** @type {{ file: string; }} */ entry) => entry.file === slug);
+
+		const module = await import(`../../../data/html/${slug}.json`);
 		const inscription = module.default;
 
 		const license = inscription.divs.at(-1);
@@ -14,8 +18,8 @@ export async function load({ params }) {
 			html: div.html.replace(/(href|src)="\/(?!\/)/g, `$1="${base}/`)
 		}));
 
-		return { inscription, license, title: inscription.title };
+		return { slug, metadata, inscription, license, title: inscription.title };
 	} catch (e) {
-		error(404, `Could not find data for ${params.slug}`);
+		error(404, `Error loading ${params.slug}: ${e.message}`);
 	}
 }
