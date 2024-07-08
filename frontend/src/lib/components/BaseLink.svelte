ecda63889e81df1b5ca4d565e631d02e2722b90f
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-07-08 12:01:46 +0100

    feat(frontend): add style for title links

--- frontend/src/lib/components/BaseLink.svelte
+++ frontend/src/lib/components/BaseLink.svelte
@@ -4,13 +4,18 @@
 	/** @type {string} */
 	export let href;
 
-	/** @type {string} */
-	export let className = '';
-
 	// ensure the href starts with a forward slash if it doesn't already
 	$: processedHref = href.startsWith('/') ? `${base}${href}` : `${base}/${href}`;
 </script>
 
-<a href={processedHref} class={className} {...$$restProps} on:click>
+<a href={processedHref} {...$$restProps} on:click>
 	<slot />
 </a>
+
+<style>
+	.title {
+		font-size: var(--font-size-4);
+		font-weight: bold;
+		margin-block: 0;
+	}
+</style>
