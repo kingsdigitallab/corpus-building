45b878970b60bb0d29c10d42129d53d3d55e93cd
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-07-16 15:13:29 +0100

    feat(frontend): add config params for search

--- frontend/src/lib/config.js
+++ frontend/src/lib/config.js
@@ -6,6 +6,10 @@ export const title = 'I.Sicily';
 export const description = `${title} is dedicated to the inscribed texts from ancient Sicily, combining an open access digital corpus with news, blogs and other resources.`;
 export const url = dev ? 'http://localhost:5173/' : publicUrl;
 
+export const search = {
+	limit: 20
+};
+
 export const imageServer =
 	'https://apheleia.classics.ox.ac.uk/iipsrv/iipsrv.fcgi?IIIF=inscription_images/';
 export const imageThumbParams = 'full/400,/0/default.jpg';
