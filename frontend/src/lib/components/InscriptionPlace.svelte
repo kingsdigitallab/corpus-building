1c460c53dddf7b4e0e3892a492849bde29b4b0b3
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-07-22 17:29:52 +0100

    feat(frontend): display both place types

--- frontend/src/lib/components/InscriptionPlace.svelte
+++ frontend/src/lib/components/InscriptionPlace.svelte
@@ -1,12 +1,28 @@
 <script>
 	export let inscription;
+
+	const mainPlace = inscription.places[0];
+	const otherPlace = inscription.places[1];
 </script>
 
 <div class="inscription-place">
-	{#if inscription.place.offset}<em>{inscription.place.offset}</em>{/if}
-	{#if inscription.place.ref}
-		<a href={inscription.place.ref}>{inscription.place._}</a>
-	{:else}
-		{inscription.place._}
-	{/if}
+	{#each [mainPlace, otherPlace] as place}
+		{#if place}
+			<span
+				>{#if place.offset}<em>{place.offset}</em>{/if}
+				{#if place.ref}
+					<a href={place.ref}>{place._}</a>
+				{:else}
+					{place._}
+				{/if}
+				({place.type})</span
+			>
+		{/if}
+	{/each}
 </div>
+
+<style>
+	span + span::before {
+		content: ', ';
+	}
+</style>
