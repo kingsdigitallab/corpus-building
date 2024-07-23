b8df4ad666344e9526956c6ba17cd8243777c467
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-07-23 17:10:23 +0100

    feat(frontend): add component to change colour scheme

---
+++ frontend/src/lib/components/ThemeToggle.svelte
@@ -0,0 +1,79 @@
+<script>
+	import { browser } from '$app/environment';
+	import { Button } from 'bits-ui';
+	import { MoonIcon, SunIcon } from 'lucide-svelte';
+
+	let theme = (browser && localStorage.getItem('color-scheme')) || 'light';
+
+	if (browser) {
+		const preference = window.matchMedia('(prefers-color-scheme: dark)');
+
+		if (preference.matches) {
+			theme = 'dark';
+		}
+
+		setTheme();
+
+		preference.addEventListener('change', (mediaQuery) => {
+			if (mediaQuery.matches) {
+				theme = 'dark';
+			} else {
+				theme = 'light';
+			}
+
+			setTheme();
+		});
+	}
+
+	function setTheme() {
+		document.documentElement.setAttribute('color-scheme', theme);
+		localStorage.setItem('color-scheme', theme);
+	}
+
+	function handleThemeToggle() {
+		theme = theme === 'light' ? 'dark' : 'light';
+
+		setTheme();
+	}
+</script>
+
+<svelte:head>
+	<script type="module">
+		let theme = localStorage.getItem('color-scheme') || 'light';
+		const preference = window.matchMedia('(prefers-color-scheme: dark)');
+
+		if (preference.matches) {
+			theme = 'dark';
+		}
+
+		document.documentElement.setAttribute('color-scheme', theme);
+		localStorage.setItem('color-scheme', theme);
+	</script>
+</svelte:head>
+
+<div class="theme-toggle">
+	<Button.Root on:click={handleThemeToggle} aria-label="Toggle colour scheme">
+		{#if theme === 'light'}
+			<MoonIcon />
+		{:else}
+			<SunIcon />
+		{/if}
+	</Button.Root>
+</div>
+
+<style>
+	.theme-toggle {
+		& button {
+			background: none;
+			border: none;
+			box-shadow: none;
+			font-weight: inherit;
+			height: 100%;
+			text-shadow: none;
+
+			&:hover {
+				background: var(--surface-4);
+			}
+		}
+	}
+</style>
