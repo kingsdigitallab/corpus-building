d134545d7cea401c0dd28198dca8095d83c055ee
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-01-24 12:38:14 +0000

    perf(inscription): move data extraction to etl pre-processing

--- frontend/src/routes/inscription/[slug]/+page.server.js
+++ frontend/src/routes/inscription/[slug]/+page.server.js
@@ -1,7 +1,5 @@
-import { base } from '$app/paths';
 import { error } from '@sveltejs/kit';
 import fs from 'fs/promises';
-import { metadataExtractors } from '@corpus-building/etl/src/metadata.js';
 import corpus from '../../../data/corpus.json';
 
 /** @type {import('../$types').PageServerLoad} */
@@ -13,32 +11,11 @@ export async function load({ params }) {
 		const metadataFileContent = await fs.readFile(metadataFilePath, 'utf8');
 		const metadata = JSON.parse(metadataFileContent);
 
-		const inscriptionFilePath = `src/data/inscriptions/${slug}.xml`;
-		let inscriptionFileContent = await fs.readFile(inscriptionFilePath, 'utf8');
-		inscriptionFileContent
-			.replaceAll(/http:\/\/sicily\.classics\.ox\.ac\.uk\/inscription\/(ISic\d{6})/g, '$1')
-			.replace(/(href|src)="\/(?!\/)/g, `$1="${base}/`);
-		const inscription = await metadataExtractors.parseXML(inscriptionFileContent);
-
 		const htmlFilePath = `src/data/html/${slug}.json`;
 		const htmlFileContent = await fs.readFile(htmlFilePath, 'utf8');
 		const html = JSON.parse(htmlFileContent);
-		const surfaces = Array.isArray(inscription.TEI.facsimile.surface)
-			? inscription.TEI.facsimile.surface
-			: [inscription.TEI.facsimile.surface];
 
-		const images = surfaces.flatMap(
-			(/** @type {{ graphic: any[]; desc: string; type: string; }} */ surface) =>
-				surface.graphic
-					.map((graphic) => ({
-						...graphic,
-						desc: graphic.desc,
-						surfaceType: surface.type
-					}))
-					.filter((image) => image.n === 'screen')
-		);
-
-		return { slug, metadata, inscription, images, html };
+		return { slug, metadata, images: metadata.graphics, html, xml: metadata.xml };
 	} catch (err) {
 		if (err instanceof Error) {
 			error(404, `Error loading ${params.slug}: ${err.message}`);
