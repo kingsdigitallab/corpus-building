ada6b2e075883054108d3c1b67900b5377c4e039
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-03-04 17:31:13 +0000

    feat(inscription): improve place display with badge-like list styling

--- frontend/src/lib/components/InscriptionPlace.svelte
+++ frontend/src/lib/components/InscriptionPlace.svelte
@@ -5,23 +5,34 @@
 </script>
 
 <div class="inscription-place">
-	{#each places as place}
-		{#if place}
-			<span
-				>{#if place.offset}<em>{place.offset}</em>{/if}
-				{#if place.ref}
-					<a href={place.ref}>{place._}</a>
-				{:else}
-					{place._}
+	<ul>
+		{#each places as place}
+			<li class="badge">
+				{#if place}
+					{#if place.offset}<em>{place.offset}</em>{/if}
+					{#if place.ref}
+						<a href={place.ref}>{place._}</a>
+					{:else}
+						{place._}
+					{/if}
+					({place.type})
 				{/if}
-				({place.type})</span
-			>
-		{/if}
-	{/each}
+			</li>
+		{/each}
+	</ul>
 </div>
 
 <style>
-	span + span::before {
-		content: ', ';
+	ul {
+		display: inline-flex;
+		gap: var(--size-2);
+		list-style: none;
+		padding: 0;
+		margin: 0;
+	}
+
+	a {
+		color: var(--text-1) !important;
+		font-weight: bold;
 	}
 </style>
