e6dfc5be355a6574ab5772400b518a28844fac83
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-03-06 15:41:23 +0000

    refactor(inscription): enhance table styling and layout
    - Add tbody to improve table structure
    - Customise table styles with updated color and spacing
    - Modify row styling with hover effects and font sizes
    - Add badges and link styling

--- frontend/src/lib/components/InscriptionTable.svelte
+++ frontend/src/lib/components/InscriptionTable.svelte
@@ -18,13 +18,32 @@
 			<th>Settlement</th>
 		</tr>
 	</thead>
-	{#each inscriptions as inscription}
-		<InscriptionTableRow {inscription} />
-	{/each}
+	<tbody>
+		{#each inscriptions as inscription}
+			<InscriptionTableRow {inscription} />
+		{/each}
+	</tbody>
 </table>
 
 <style>
 	table {
+		--nice-inner-radius: 0;
+
+		background: unset;
+		border: unset;
+		border-radius: unset;
+		border-spacing: 0;
+		border-top: 1px solid var(--border-color);
 		width: 100%;
+	}
+
+	thead {
+		border-start-start-radius: unset;
+		background-color: var(--surface-2);
+		font-size: var(--font-size-1);
+	}
+
+	th {
+		text-align: left;
 	}
 </style>
--- frontend/src/lib/components/InscriptionTableRow.svelte
+++ frontend/src/lib/components/InscriptionTableRow.svelte
@@ -7,25 +7,59 @@
 </script>
 
 <tr>
-	<td><InscriptionLink id={inscription.file}>{inscription.file}</InscriptionLink></td>
-	<td><InscriptionLink id={inscription.file}>{inscription.title}</InscriptionLink></td>
+	<td class="strong">
+		<InscriptionLink id={inscription.file}>{inscription.file}</InscriptionLink>
+	</td>
+	<td class="strong">
+		<InscriptionLink id={inscription.file}>{inscription.title}</InscriptionLink>
+	</td>
 	<td><InscriptionDate date={inscription.date} /></td>
 	<td><InscriptionPlace {inscription} /></td>
 	<td>{inscription.status}</td>
 	<td>
 		{#if inscription.type?.ref}
-			<a href={inscription.type.ref}>{inscription.type?._}</a>
+			<a class="badge" href={inscription.type.ref}>{inscription.type?._}</a>
 		{:else}
 			{inscription.type?._ || 'N/A'}
 		{/if}
 	</td>
 	<td>
-		{#if inscription.objectType?.ref}
-			<a href={inscription.objectType.ref}>{inscription.objectType?._}</a>
+		{#if inscription.rawObjectType?.ref}
+			<a class="badge" href={inscription.rawObjectType.ref}>{inscription.rawObjectType?._}</a>
 		{:else}
-			{inscription.objectType?._ || 'N/A'}
+			{inscription.rawObjectType?._ || 'N/A'}
 		{/if}
 	</td>
 	<td>{inscription.textLang?._ || 'N/A'}</td>
 	<td>{inscription.settlement || 'N/A'}</td>
 </tr>
+
+<style>
+	tr {
+		font-size: var(--font-size-0);
+		vertical-align: top;
+		padding-block: var(--size-1);
+	}
+
+	tr:hover {
+		font-weight: 600;
+	}
+
+	td {
+		padding-block: var(--size-4);
+		text-align: left;
+	}
+
+	td:has(.badge)::first-letter {
+		text-transform: capitalize;
+	}
+
+	:global(td a, td a:visited) {
+		color: var(--text-1);
+	}
+
+	:global(td .inscription-date),
+	:global(td .inscription-place li) {
+		font-size: var(--font-size-0);
+	}
+</style>
