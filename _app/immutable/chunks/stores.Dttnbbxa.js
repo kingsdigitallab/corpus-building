c5e7475541455a2740a7765ea0a0665dc1325ab4
jmiguelv <jmiguelv@users.noreply.github.com>
2025-03-19 14:22:25 +0000

    deploy: 8c7f7e0872173bd6e971d071d32894ec2efaa73f

---
+++ _app/immutable/chunks/stores.Dttnbbxa.js
@@ -0,0 +1,1 @@
+import{s as e}from"./entry.8Dp6RRvB.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
