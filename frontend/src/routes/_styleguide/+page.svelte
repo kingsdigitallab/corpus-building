91cb8c1085ef822eabdf4406b2c196c3e71ccb7d
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-03-23 16:09:09 +0000

    feat(styleguide): add select component to style guide

--- frontend/src/routes/_styleguide/+page.svelte
+++ frontend/src/routes/_styleguide/+page.svelte
@@ -91,6 +91,16 @@
 								<input type="text" />
 							</label>
 						</div>
+						<div>
+							<label>
+								<span>Label</span>
+								<select>
+									<option value="1">Option 1</option>
+									<option value="2">Option 2</option>
+									<option value="3">Option 3</option>
+								</select>
+							</label>
+						</div>
 					</div>
 				</section>
 			</section>
