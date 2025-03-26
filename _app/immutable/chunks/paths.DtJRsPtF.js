7359c2353458f9845481976f7689cb3e459b593f
jmiguelv <jmiguelv@users.noreply.github.com>
2025-03-26 22:27:04 +0000

    deploy: 708604b4db302b58679767d8e23c9c04c8d0e8cf

---
+++ _app/immutable/chunks/paths.DtJRsPtF.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_ec89it)==null?void 0:s.base)??"/corpus-building";var e;const a=((e=globalThis.__sveltekit_ec89it)==null?void 0:e.assets)??t;export{a,t as b};
