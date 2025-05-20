1457ccdd18ec9d653dbe59a787c714bd9e9bce15
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-05-20 15:02:42 +0100

    feat(landing): implement hero section according to design

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,23 +1,109 @@
 <script>
 	import * as config from '$lib/config';
 	import FacetedSearch from '$lib/components/search/FacetedSearch.svelte';
+	import heroImage from '$lib/assets/images/hero.png';
 </script>
 
 <section class="hero">
-	<h1>{config.description}</h1>
+	<div class="hero-left">
+		<hgroup>
+			<h1>{config.title}</h1>
+			<h2>{config.description}</h2>
+		</hgroup>
+		<a class="discover-link" href="#faceted-search">
+			<span class="arrow">↓</span>
+			<span class="discover-link-text">Discover all inscriptions</span>
+		</a>
+	</div>
+	<div class="hero-right">
+		<picture>
+			<img src={heroImage} alt="Hero image" />
+		</picture>
+	</div>
 </section>
+
+<div class="hero-spacer"></div>
 
 <FacetedSearch />
 
 <style>
 	.hero {
+		border-bottom: var(--border-size-1) solid var(--border-color);
 		display: grid;
-		place-items: center;
+		grid-template-columns: 1fr 1fr;
+		margin: 0;
+		min-height: calc(100vh - 140px);
 	}
 
-	.hero h1 {
-		font-size: var(--font-size-4);
-		max-inline-size: var(--header-size-4);
-		text-align: center;
+	.hero-left {
+		align-items: flex-start;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		padding-block: var(--size-6);
+	}
+
+	.hero-left h1 {
+		font-size: clamp(4rem, 8vw, 7rem);
+		font-weight: 900;
+		text-transform: uppercase;
+		letter-spacing: 0.02em;
+	}
+
+	.hero-left h2 {
+		font-weight: 600;
+		max-inline-size: 100%;
+	}
+
+	.discover-link {
+		align-items: center;
+		color: var(--text-1);
+		display: flex;
+		font-size: var(--font-size-2);
+		font-weight: bold;
+		gap: var(--size-2);
+		text-decoration: none;
+	}
+
+	.arrow {
+		border-radius: var(--radius);
+		border: var(--border-size-1) solid var(--border-color);
+		padding-inline: var(--size-2);
+	}
+
+	.discover-link-text {
+		text-decoration: underline;
+	}
+
+	.hero-right {
+		align-self: stretch;
+		border-left: var(--border-size-1) solid var(--border-color);
+		overflow: visible;
+		z-index: -1;
+	}
+
+	.hero-right img {
+		display: block;
+		height: 110%;
+		object-fit: cover;
+		object-position: left;
+		width: 100%;
+	}
+
+	.hero-spacer {
+		height: var(--size-10);
+	}
+
+	@media (max-width: 768px) {
+		.hero {
+			grid-template-columns: 1fr;
+			height: auto;
+		}
+		.hero-right {
+			display: none;
+		}
+		.hero-spacer {
+			height: var(--size-6);
+		}
 	}
 </style>
