7cb5afe867f10c8211f9383a5faf11713c5ed22d
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-05-23 16:15:15 +0100

    feat(museum): add museum detail page

---
+++ frontend/src/routes/museum/[slug]/+page.server.js
@@ -0,0 +1,41 @@
+import { error } from '@sveltejs/kit';
+import corpus from '../../../data/corpus.json';
+import museums from '../../../data/museums.json';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ params: { slug } }) {
+	try {
+		/**
+		 * @type {{
+		 * 	slug: string,
+		 * 	uri: string,
+		 * 	name: string,
+		 * 	type: string,
+		 * 	description: string,
+		 * 	location: {
+		 * 		address: string,
+		 * 		settlement: string,
+		 * 		region: string,
+		 * 		country: string,
+		 * 		geo: { lon: number, lat: number }
+		 * 	}
+		 * } | undefined}
+		 */
+		const museum = museums.find((museum) => museum.slug === slug);
+
+		if (!museum) {
+			throw error(404, `Museum ${slug} not found`);
+		}
+
+		const inscriptions = /** @type {any[]} */ (corpus).filter(
+			(inscription) => inscription?.repository?.ref === museum.uri
+		);
+
+		return { museum, inscriptions };
+	} catch (e) {
+		if (e instanceof Error) {
+			error(404, `Error loading museum ${slug}: ${e.message}`);
+		}
+		throw error(404, `Error loading museum ${slug}`);
+	}
+}
---
+++ frontend/src/routes/museum/[slug]/+page.svelte
@@ -0,0 +1,72 @@
+<script>
+	import InscriptionTable from '$lib/components/InscriptionTable.svelte';
+	import * as config from '$lib/config';
+	import { LucideExternalLink } from 'lucide-svelte';
+	import { DefaultMarker, MapLibre, Popup } from 'svelte-maplibre';
+
+	/** @type {{ data: import('./$types').PageData }} */
+	const { data } = $props();
+	const { museum, inscriptions } = data;
+</script>
+
+<svelte:head>
+	<title>{museum.name} | {config.title}</title>
+	<meta name="description" content={museum.name} />
+	<meta name="tags" content="museum, inscriptions, {museum.name}" />
+</svelte:head>
+
+{#snippet address(/** @type {import('./$types').PageData['museum']['location']} */ location)}
+	<address>
+		<p>{location.address}</p>
+		<p>{location.settlement}, {location.region}, {location.country}</p>
+	</address>
+{/snippet}
+
+<article>
+	<header>
+		<hgroup>
+			<h1>
+				{museum.name}
+				<small class="badge">{museum.type}</small>
+			</h1>
+			<p class="description">
+				{museum.description}
+			</p>
+			<a href={museum.uri} target="museum">
+				View in current site <LucideExternalLink />
+			</a>
+		</hgroup>
+	</header>
+
+	<section>
+		<h2>Location</h2>
+		{@render address(museum.location)}
+		{#if museum.location.geo}
+			{@const geo = museum.location.geo}
+			{@const lngLat = [geo.lon, geo.lat]}
+			<MapLibre center={lngLat} zoom={7} class="map" standardControls style={config.mapStyle}>
+				<DefaultMarker {lngLat}>
+					<Popup offset={[0, -10]}>
+						<div class="popup">
+							<h3>{museum.name}</h3>
+							{@render address(museum.location)}
+						</div>
+					</Popup>
+				</DefaultMarker>
+			</MapLibre>
+		{/if}
+	</section>
+
+	<section>
+		<h2>Inscriptions</h2>
+		<InscriptionTable {inscriptions} />
+	</section>
+</article>
+
+<style>
+	header a {
+		align-items: center;
+		display: inline-flex;
+		gap: var(--size-2);
+	}
+</style>
