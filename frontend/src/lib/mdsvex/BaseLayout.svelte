662beff3c06e2f2e4dac4d1a1ce9e17ce3f9f395
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-06-10 15:51:04 +0100

    feat(base-layout): implement centred layout

--- frontend/src/lib/mdsvex/BaseLayout.svelte
+++ frontend/src/lib/mdsvex/BaseLayout.svelte
@@ -10,7 +10,28 @@
 	<meta name="tags" content={tags.join(', ')} />
 </svelte:head>
 
-<article>
+<article class="md">
 	<h1>{title}</h1>
 	<slot />
 </article>
+
+<style>
+	.md {
+		display: flex;
+		flex-direction: column;
+		gap: var(--size-4);
+		margin-block: var(--size-8);
+		margin-inline: auto;
+		max-width: 80ch;
+	}
+
+	.md h1 {
+		max-width: 100%;
+		text-align: center;
+	}
+
+	:global(.md p) {
+		max-inline-size: unset;
+		text-align: justify;
+	}
+</style>
