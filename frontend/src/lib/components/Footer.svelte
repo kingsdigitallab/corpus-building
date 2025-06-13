a6398a3077b0eeead64620c0b57b98721524c38c
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-06-13 16:45:04 +0100

    feat(footer): add footer component with navigation, social media links, policies, and logos

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -1,29 +1,170 @@
 <script>
+	import { base } from '$app/paths';
 	import * as config from '$lib/config';
+	import { footer } from '$lib/footer';
 	import BaseLink from '$lib/components/BaseLink.svelte';
 
 	const version = import.meta.env.APP_VERSION;
 </script>
 
 <footer>
-	<BaseLink href="/">{config.title}</BaseLink>
-
-	<a
-		class="version"
-		href="https://github.com/kingsdigitallab/corpus-building/blob/v{version}/CHANGELOG.md"
-		>v{version}</a
-	>
+	<section class="footer-links">
+		{#each footer.links as section}
+			<div class="footer-link-section">
+				<h3>{section.title}</h3>
+				<ul>
+					{#each section.links as link}
+						<li class={link.class || ''}>
+							{#if link.href}
+								{#if link.href.startsWith('http') || link.href.startsWith('mailto')}
+									<a href={link.href}>{link.title}</a>
+								{:else}
+									<BaseLink href={link.href}>{link.title}</BaseLink>
+								{/if}
+							{:else}
+								{link.title}
+							{/if}
+						</li>
+					{/each}
+				</ul>
+			</div>
+		{/each}
+	</section>
+	<section class="footer-logos">
+		<ul>
+			{#each footer.logos as logo}
+				<li>
+					{#if logo.href}
+						<a href={logo.href}>
+							<img src={`${base}${logo.img}`} alt={logo.title} width={logo.width || 75} />
+						</a>
+					{:else}
+						<img src={`${base}${logo.img}`} alt={logo.title} width={logo.width || 75} />
+					{/if}
+				</li>
+			{/each}
+		</ul>
+	</section>
+	<section class="footer-bottom">
+		<ul>
+			<li>
+				<BaseLink href="/">{config.title}</BaseLink>
+			</li>
+			<li class="kdl">
+				Design, developed and maintained by <a href="https://kdl.kcl.ac.uk/">King's Digital Lab</a>
+			</li>
+			<li>
+				<a
+					class="version"
+					href="https://github.com/kingsdigitallab/corpus-building/blob/v{version}/CHANGELOG.md"
+					>v{version}</a
+				>
+			</li>
+		</ul>
+	</section>
 </footer>
 
 <style>
 	footer {
 		border-top: var(--border-size-1) solid var(--border-color);
 		display: flex;
+		flex-direction: column;
 		justify-content: space-between;
-		padding-block: var(--size-4);
 	}
 
-	.version {
+	.footer-links {
+		display: flex;
+		gap: var(--size-6);
+		justify-content: space-around;
+		margin-block: 0;
+		padding-block-end: var(--size-4);
+		padding-block-start: var(--size-10);
+	}
+
+	@media (max-width: 768px) {
+		.footer-links {
+			align-items: center;
+			flex-direction: column;
+			gap: var(--size-4);
+		}
+	}
+
+	.footer-link-section {
+		padding-inline: var(--size-6);
+	}
+
+	@media (max-width: 768px) {
+		.footer-link-section {
+			text-align: center;
+		}
+	}
+
+	.footer-link-section h3 {
+		font-size: var(--font-size-2);
+		font-weight: 600;
+		margin-block-end: var(--size-6);
+	}
+
+	.footer-logos {
+		border-top: var(--border-size-1) solid var(--border-color);
+		border-bottom: var(--border-size-1) solid var(--border-color);
+		margin-block: 0;
+		padding-block: var(--size-6);
+	}
+
+	.footer-logos ul {
+		align-items: center;
+		display: flex;
+		justify-content: space-between;
+		gap: var(--size-6);
+	}
+
+	@media (max-width: 768px) {
+		.footer-logos ul {
+			gap: var(--size-4);
+			flex-wrap: wrap;
+			justify-content: center;
+			max-width: 50%;
+			margin: 0 auto;
+		}
+	}
+
+	ul,
+	li {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	li.small {
 		font-size: var(--font-size-0);
+	}
+	li.spacer {
+		margin-block-start: var(--size-10);
+	}
+
+	@media (max-width: 768px) {
+		li.spacer {
+			margin-block-start: var(--size-2);
+		}
+	}
+
+	.footer-bottom ul {
+		display: flex;
+		font-size: var(--font-size-0);
+		justify-content: space-between;
+		gap: var(--size-6);
+	}
+
+	@media (max-width: 768px) {
+		.footer-bottom ul {
+			align-items: center;
+			flex-direction: column;
+			gap: var(--size-4);
+		}
+	}
+
+	.kdl a {
+		text-decoration: underline;
 	}
 </style>
---
+++ frontend/src/lib/footer.js
@@ -0,0 +1,136 @@
+export const footer = {
+	links: [
+		{
+			title: 'Navigation Links',
+			links: [
+				{
+					title: 'Home',
+					href: '/'
+				},
+				{
+					title: 'About',
+					href: '/about'
+				},
+				{
+					title: 'Guide',
+					href: ''
+				},
+				{
+					title: 'FAQ',
+					href: ''
+				},
+				{
+					title: 'Back to Top',
+					href: '#top',
+					class: 'small spacer'
+				}
+			]
+		},
+		{
+			title: 'Social Media Links',
+			links: [
+				{
+					title: 'GitHub',
+					href: 'https://github.com/ISicily'
+				},
+				{
+					title: 'Facebook',
+					href: 'https://www.facebook.com/ISicily'
+				},
+				{
+					title: 'Twitter',
+					href: 'https://twitter.com/ISicily'
+				}
+			]
+		},
+		{
+			title: 'Policy',
+			links: [
+				{
+					title: 'Terms & Policies',
+					href: 'https://www.ox.ac.uk/legal'
+				},
+				{
+					title: 'Privacy Statement',
+					href: 'https://www.ox.ac.uk/privacy-policy'
+				},
+				{
+					title: 'Accessibility',
+					href: ''
+				},
+				{
+					title: 'Credits & Acknowledgements',
+					href: ''
+				},
+				{
+					title: 'Cookies',
+					href: 'https://www.ox.ac.uk/cookies-privacy-policy'
+				}
+			]
+		},
+		{
+			title: 'More Info',
+			links: [
+				{
+					title: 'News and Blog',
+					href: 'https://isicily.org/'
+				},
+				{
+					title: 'Officina IG XIV2',
+					href: 'https://www.officina-igxiv2.org/home-page'
+				}
+			]
+		},
+		{
+			title: 'Contact',
+			links: [
+				{
+					title: 'Email',
+					href: 'mailto:jonathan.prag@classics.ox.ac.uk'
+				},
+				{
+					title: 'Feedback',
+					href: ''
+				}
+			]
+		}
+	],
+	logos: [
+		{
+			title: 'Università di Catania',
+			href: 'https://unict.it/',
+			img: '/images/logos/UniCT-Logo-Nero.png'
+		},
+		{
+			title: 'University of Oxford',
+			href: 'https://ox.ac.uk/',
+			img: '/images/logos/2258_ox_brand_blue_pos_rect.png'
+		},
+		{
+			title: 'I.Sicily',
+			href: 'https://isicily.org/',
+			img: '/images/logos/ISicily_english_reduced.jpg'
+		},
+		{
+			title: "King's College London",
+			href: 'https://kcl.ac.uk/',
+			img: '/images/logos/kings-logo-red.svg',
+			width: 75
+		},
+		{
+			title: 'Regione Siciliana',
+			href: 'https://regione.sicilia.it/',
+			img: '/images/logos/RegioneSiciliana.png'
+		},
+		{
+			title: 'European Research Council',
+			href: 'https://erc.europa.eu/',
+			img: '/images/logos/LOGO_ERC-FLAG_EU_cropped.jpg'
+		},
+		{
+			title: 'CROSSREADS',
+			href: 'https://crossreads.web.ox.ac.uk/',
+			img: '/images/logos/Crossreads_800_nobackground.png'
+		}
+	]
+};
