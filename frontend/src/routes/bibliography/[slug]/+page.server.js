466c2178f1d5bbacf642bb48b19a63426a60b1d4
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-08-20 16:18:54 +0100

    feat(bibliography): Add bibliography route

---
+++ frontend/src/routes/bibliography/[slug]/+page.server.js
@@ -0,0 +1,25 @@
+import { error } from '@sveltejs/kit';
+import corpus from '../../../data/corpus.json';
+import zotero from '../../../data/zotero.json';
+
+/** @type {import('../$types').PageServerLoad} */
+export async function load({ params: { slug } }) {
+	try {
+		const z = zotero[slug];
+
+		if (!z) {
+			throw error(404, `Zotero item ${slug} not found`);
+		}
+
+		const inscriptions = corpus.filter((/** @type {{ zotero: string | any[]; }} */ inscription) =>
+			inscription.zotero?.includes(slug)
+		);
+
+		return { zotero: z, inscriptions };
+	} catch (e) {
+		if (e instanceof Error) {
+			error(404, `Error loading bibliography: ${e.message}`);
+		}
+		throw error(404, `Error loading bibliography ${slug}`);
+	}
+}
---
+++ frontend/src/routes/bibliography/[slug]/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+	import InscriptionTable from '$lib/components/InscriptionTable.svelte';
+	import * as config from '$lib/config';
+	import { LucideExternalLink } from 'lucide-svelte';
+
+	/** @type {{ data: import('./$types').PageData }} */
+	const { data } = $props();
+	const { zotero, inscriptions } = data;
+</script>
+
+<svelte:head>
+	<title>{zotero.title} | {config.title}</title>
+	<meta name="description" content={zotero.title} />
+	<meta name="tags" content="bibliography, {zotero.title}" />
+</svelte:head>
+
+<article>
+	<header>
+		<hgroup>
+			<h1>{zotero.title}</h1>
+			<p class="description">{@html zotero.citation}</p>
+			<a href={zotero.uri} target="zotero">
+				View in Zotero <LucideExternalLink />
+			</a>
+		</hgroup>
+	</header>
+
+	<InscriptionTable {inscriptions} />
+</article>
+
+<style>
+	header a {
+		align-items: center;
+		display: inline-flex;
+		gap: var(--size-2);
+	}
+</style>
