4fa30e0750de2f97af69452b738a692510e35877
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-09-16 09:50:41 +0100

    feat(inscription-material): Add component to render material information

---
+++ frontend/src/lib/components/InscriptionMaterial.svelte
@@ -0,0 +1,14 @@
+<script>
+	let { material } = $props();
+</script>
+
+<span class="inscription-material">
+	{#if material}
+		{#if Array.isArray(material)}
+			{material.slice(-1)[0].split(':::').slice(-1)[0]}
+		{:else if material?._}
+			{material._}
+		{/if}
+	{:else}'N/A'
+	{/if}
+</span>
