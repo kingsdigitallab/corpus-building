f1bd2754d6166671394958f0676865e871ce02f1
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-09-16 10:30:16 +0100

    fix(inscription-date): Ensure date updates on data changes

--- frontend/src/lib/components/InscriptionDate.svelte
+++ frontend/src/lib/components/InscriptionDate.svelte
@@ -1,21 +1,25 @@
 <script>
 	let { date } = $props();
 
-	let dates = [];
+	function getDateString() {
+		let dates = [];
 
-	for (const key of ['notBefore', 'notAfter']) {
-		if (date[key]) {
-			if (date[key] < 0) {
-				dates.push(`${Math.abs(date[key])} BC`);
+		for (const key of ['notBefore', 'notAfter']) {
+			if (date[key]) {
+				if (date[key] < 0) {
+					dates.push(`${Math.abs(date[key])} BC`);
+				} else {
+					dates.push(`AD ${date[key]}`);
+				}
 			} else {
-				dates.push(`AD ${date[key]}`);
+				dates.push('Unknown');
 			}
-		} else {
-			dates.push('Unknown');
 		}
+
+		return dates.join(' - ');
 	}
 
-	let dateString = $derived(dates.join(' – '));
+	let dateString = $derived(getDateString());
 </script>
 
 <span class="inscription-date">{dateString}</span>
