969e45969df44f7de94dc71079a62194a9e198fd
Zihao-kdl <zihao.lu@kcl.ac.uk>
2025-11-06 11:21:49 +0000

    refactor(inscriptionlist): Adjust layout and spacing for better responsiveness

--- frontend/src/lib/components/InscriptionList.svelte
+++ frontend/src/lib/components/InscriptionList.svelte
@@ -18,7 +18,7 @@
 		flex-direction: row;
 		flex-wrap: wrap;
 		gap: var(--size-1);
-		justify-content: space-between;
+		justify-content: flex-start; /* ZL changed from space-between */
 		list-style: none;
 		padding-inline-start: 0;
 
@@ -32,16 +32,22 @@
 			}
 
 			@media (min-width: 640px) {
-				flex-basis: 45%;
+				flex-basis: calc(50% - (var(--size-2) / 2));
 			}
 
 			@media (min-width: 1024px) {
-				flex-basis: 33%;
+				flex-basis: calc(33.3333% - (2 * var(--size-2) / 3));
 			}
 		}
 
 		& li:hover {
 			box-shadow: var(--shadow-4);
+		}
+	}
+
+	@media (min-width: 640%) and (max-width: 1023.98px) {
+		ol {
+			gap: var(--size-2);
 		}
 	}
 </style>
