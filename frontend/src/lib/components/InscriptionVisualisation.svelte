95f4d3b5b572d4edbcbb717eba49034714d2b527
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2025-12-15 19:31:46 +0800

    feat(viz): Add InscriptionVisualisation component
    Provide UI to switch view (map/bar/donut), choose category and
    colour-by. Build categories from aggregations, bind state for controls,
    render InscriptionMap for map view and placeholders for other views.

---
+++ frontend/src/lib/components/InscriptionVisualisation.svelte
@@ -0,0 +1,66 @@
+<script>
+	import InscriptionMap from './InscriptionMap.svelte';
+
+	let { inscriptions, aggregations } = $props();
+
+	let selectedView = $state('map');
+	let selectedCategory = $state('inscriptionType');
+	let selectedColourBy = $state('inscriptionType');
+
+	const categories = Object.values(aggregations).map((aggregation) => ({
+		value: aggregation.name,
+		label: aggregation.title
+	}));
+</script>
+
+<section id="viz-controls">
+	<fieldset>
+		<label>
+			View
+			<select name="view" bind:value={selectedView}>
+				<option value="bar-stacked">Bar</option>
+				<option value="donut">Donut</option>
+				<option value="map" selected>Map</option>
+			</select>
+		</label>
+		<label>
+			Category
+			<select name="category" bind:value={selectedCategory}>
+				{#each categories as category (category.value)}
+					<option value={category.value}>{category.label}</option>
+				{/each}
+			</select>
+		</label>
+		<label>
+			Colour by
+			<select name="colourBy" bind:value={selectedColourBy}>
+				{#each categories as category (category.value)}
+					{#if category.value === selectedCategory}
+						<option value={category.value} disabled>{category.label}</option>
+					{:else}
+						<option value={category.value}>{category.label}</option>
+					{/if}
+				{/each}
+			</select>
+		</label>
+	</fieldset>
+</section>
+
+<section id="viz-container">
+	{#if selectedView === 'map'}
+		<InscriptionMap {inscriptions} />
+	{:else if selectedView === 'bar-stacked'}
+		<p>The bars are stacked!</p>
+	{:else if selectedView === 'donut'}
+		<p>🍩</p>
+	{:else}
+		<code>If you are seeing this, something went wrong!</code>
+	{/if}
+</section>
+
+<style>
+	#viz-controls fieldset {
+		display: flex;
+		justify-content: space-around;
+	}
+</style>
