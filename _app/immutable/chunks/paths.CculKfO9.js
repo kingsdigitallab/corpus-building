d71822a672c64b69cf7107eaced837786598700d
jmiguelv <jmiguelv@users.noreply.github.com>
2025-12-17 14:46:18 +0000

    deploy: 203d21a2dab5deeacb541c52234236a6c8c20b58

---
+++ _app/immutable/chunks/paths.CculKfO9.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_6z0g2m)==null?void 0:s.base)??"/corpus-building";var a;const t=((a=globalThis.__sveltekit_6z0g2m)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ _app/immutable/entry/start.qBjMbPVX.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DdO0x2TC.js";export{t as start};
