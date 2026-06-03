eeeb4cebcb3e973b40ebce7d828dd25a9fcfdf07
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2026-06-03 22:43:40 +0100

    fix(inscription): Meterial subtypes now separated with comma and space

--- frontend/src/lib/components/InscriptionMaterialDetail.svelte
+++ frontend/src/lib/components/InscriptionMaterialDetail.svelte
@@ -16,14 +16,14 @@
 			{#if material?.ref}
 				<a class="badge strong" href={material.ref}>
 					{material.type}
-					{#if material.subtype}
-						> {material.subtype}
+					{#if material.subtype && material.subtype.length }
+						> {material.subtype.join(', ')}
 					{/if}
 				</a>
 			{:else}
 				{material.type}
-				{#if material.subtype}
-					> {material.subtype}
+				{#if material.subtype && material.subtype.length }
+					> {material.subtype.join(', ')}
 				{/if}
 			{/if}
 		</dd>
